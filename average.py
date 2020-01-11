3813ee3698de1bac006856a00406a48cb2739c51
Joel Chenoweth <42840168+jchenwth101@users.noreply.github.com>
2020-01-10 22:18:04 -0500

    Update average.py

--- average.py
+++ average.py
@@ -1,4 +1,4 @@
-print("Please enter five numbers. ")
+print("Please enter five numbers.")
 num_1 = float(input())
 num_2 = float(input())
 num_3 = float(input())
@@ -6,5 +6,5 @@ num_4 = float(input())
 num_5 = float(input())
 average = num_1 + num_2 + num_3 + num_4 + num_5
 mean = average / 5
-print("The average of those numbers is: ")
+print("The average of those numbers is:")
 print(mean)
