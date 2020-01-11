21c3a73f44ebc6e4e4c92ae148f18112b60e64e2
Joel Chenoweth <42840168+jchenwth101@users.noreply.github.com>
2020-01-10 22:10:21 -0500

    Update average.py

--- average.py
+++ average.py
@@ -6,4 +6,5 @@ num_4 = float(input())
 num_5 = float(input())
 average = num_1 + num_2 + num_3 + num_4 + num_5
 mean = average / 5
-print("The average of those numbers is: {} ".format(mean))
+print("The average of those numbers is: ")
+print(mean)
